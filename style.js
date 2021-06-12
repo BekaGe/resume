91d8e467db7eecb0a66a08363806dd4ef01257f4
BekaGe <85070078+BekaGe@users.noreply.github.com>
2021-06-12 16:45:45 +0400

    without responsive

---
+++ style.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [  "Designer","Developer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+$('.navbar .menu li a').click(function(){
+    $('html').css("scrollBehavior", "smooth");
+});
