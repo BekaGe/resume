47c2056ab37f65256f996cbe39f01139ac6a56b1
BekaGe <85070078+BekaGe@users.noreply.github.com>
2021-06-26 15:53:18 +0400

    navbar-sticky

--- style.js
+++ style.js
@@ -32,3 +32,13 @@ $(document).ready(function(){
 $('.navbar .menu li a').click(function(){
     $('html').css("scrollBehavior", "smooth");
 });
+
+   $(window).scroll(function(){
+       if(this.scrollY > 20){
+           $('.navbar').addClass('sticky')
+       }
+       else{
+           $('.navbar').removeClass('sticky')
+       }
+   })
+
