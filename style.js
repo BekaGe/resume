4479e4510e55e37bafe34b6abb3d25a9e75a8cc4
BekaGe <85070078+BekaGe@users.noreply.github.com>
2021-07-01 10:54:09 +0400

    non responsive

--- style.js
+++ style.js
@@ -7,38 +7,19 @@ $(document).ready(function(){
         backSpeed: 80,
         loop: true
     });
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
+   
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky')
         }
+        else{
+            $('.navbar').removeClass('sticky')
+        }
+    })
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
     });
 });
-$('.navbar .menu li a').click(function(){
-    $('html').css("scrollBehavior", "smooth");
-});
 
-   $(window).scroll(function(){
-       if(this.scrollY > 20){
-           $('.navbar').addClass('sticky')
-       }
-       else{
-           $('.navbar').removeClass('sticky')
-       }
-   })
+
 
